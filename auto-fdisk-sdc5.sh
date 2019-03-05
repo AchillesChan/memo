f1dd1a82d79d9a476a9319dfe35f94ac8b805748
Michael <xt833a@hotmail.com>
2019-03-05 10:30:34 +0800

    update staff

---
+++ auto-fdisk-sdc5.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -u
+devicePath="/dev/sdc"
+filePath="/data"
+devicePartition="/dev/sdc5"
+fstabFile="/etc/fstab"
+fileType=ext4
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' <<eof |fdisk "$devicePath"
+n #new
+e #extend
+  # space
+  # space
+  # space
+n #logical
+l #logical
+  # space
+  # space
+w #
+eof
+
+
+[[ -d "$filePath" ]] || mkdir -p "$filePath"
+mkfs."$fileType" "$devicePartition"
+deviceUUID=$(blkid|grep "$devicePartition"|awk '{print $2}'|sed  's:"::g')
+
+cp "$fstabFile" /tmp/                                         
+echo "$deviceUUID    "$filePath"    "$fileType"     defaults       0 0" >>"$fstabFile"
+
+mount -a
+
