332275abbafab32e8d0ebf350ca8b6e0b7fec756
Michael <xt833a@hotmail.com>
2020-04-15 15:08:45 +0800

    update

--- detect-ca-expire-send-mail.sh
+++ detect-ca-expire-send-mail.sh
@@ -1,18 +1,23 @@
 [root@host user]# cat sites
 #SITE                               TAG
-www.abcyc.com                        WAN2.8.6
-an.abc.link                          139.359.7.3
+www.abc.com                         rp-abc
+an.abc.link                         rp-2.5.5
 
 
 
 [root@host user]# cat detect-ca.sh
 #!/bin/bash
+#_DEBUG="on"
+
+function DEBUG()
+ {
+      [ "$_DEBUG" == "on" ] &&  $@ || :
+  }
+
 PATH="/usr/local/bin:/usr/bin:/bin"
 SITELIST="/home/Achilles/sites"
 LOGFILE="/home/Achilles/siteca"
-MIN_DAYS=99
-
-
+TIMEOUT=5
 
 while IFS= read -r SITE
 do
@@ -21,21 +26,23 @@ do
         continue
     fi
 
-    URL=$(echo "$SITE"|awk '{print $1}')
-    TAG=$(echo "$SITE"|awk '{print $2}')
-
-
-curl -kvLI $URL 1>/dev/null 2>$LOGFILE
-
-DATE_THEN=$(date -d "$(grep "expire date" "$LOGFILE" |sed -e 's/^.*date://g')" +%s)
-DATE_NOW=$(date +%s)
-DATE_DIFFERENCE=$((($DATE_THEN - $DATE_NOW)/86400))
-
-if [ $DATE_DIFFERENCE -le $MIN_DAYS ]
+URL=$(echo "$SITE"|awk '{print $1}')
+TAG=$(echo "$SITE"|awk '{print $2}')
+MIN_DAYS=$(echo "$SITE"|awk '{print $3}')
+if curl -kvLI -ILkv --connect-timeout "$TIMEOUT" https://$URL 2>$LOGFILE 1>/dev/null
 then
-    mail -s "$(hostname) $(date) $URL $TAG cert only $DATE_DIFFERENCE DAYS" mail_user@abc.link </dev/null
-fi
 
+        DATE_THEN=$(date -d "$(grep "expire date" "$LOGFILE" |sed -e 's/^.*date://g')" +%s)
+        DATE_NOW=$(date +%s)
+
+        DATE_DIFFERENCE=$((($DATE_THEN - $DATE_NOW)/86400))
+        if [[ "$DATE_DIFFERENCE" -le "$MIN_DAYS" ]]
+        then
+            mail -s "$(hostname) $(date) $URL $TAG cert only $DATE_DIFFERENCE DAYS" mail_user@abc.com </dev/null
+        fi
+else
+        mail -s "$(hostname) $(date) https://$URL 没有响应 " mail_user@abc.com </dev/null
+fi
 done <"$SITELIST"
 wait
 
