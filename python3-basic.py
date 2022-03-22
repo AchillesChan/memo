4c672ac1a67a913c432715ac0f9eceec7bea34e7
Michael <xt833a@hotmail.com>
2022-03-22 13:46:18 +0800

    update

--- python3-basic.py
+++ python3-basic.py
@@ -1,3 +1,5 @@
+''' basic
+
 # -*- coding: utf-8 -*-
 
 import os
@@ -48,95 +50,121 @@ def foo(p1,p2):
 if __name__ == '__main__':
     main()
 
+'''
 
-##oop###01 # -*- coding: utf-8 -*-
-##oop###01 import os
-##oop###01 class Student(object):
-##oop###01     def __init__(self,name,score):
-##oop###01         self.name = name
-##oop###01         self.score =score
-##oop###01     def set_score(self,score):
-##oop###01         self.score =score
-##oop###01     def print_score(self):
-##oop###01         print('Student %s\'s score is %s' %(self.name,self.score))
+
+
+
+'''oop program
+
+# -*- coding: utf-8 -*-
+import os
+class Student(object):
+    def __init__(self,name,score):
+        self.name = name
+        self.score =score
+    def set_score(self,score):
+        self.score =score
+    def print_score(self):
+        print('Student %s\'s score is %s' %(self.name,self.score))
 ##oop###01
-##oop###01 def OOP01MAIN():
-##oop###01     foo = Student('foo',66)
-##oop###01     foo.print_score()
-##oop###01     foo.set_score(91)
-##oop###01     foo.print_score()
+def OOP01MAIN():
+    foo = Student('foo',66)
+    foo.print_score()
+    foo.set_score(91)
+    foo.print_score()
 ##oop###01
-##oop###01 if __name__ == '__main__':
-##oop###01     OOP01MAIN()
-##oop###01
+if __name__ == '__main__':
+    OOP01MAIN()
 
-
-
-###copy-file-op01### #https://docs.python.org/3/library/shutil.html
-###copy-file-op01### #https://www.geeksforgeeks.org/python-shutil-copy-method/
-###copy-file-op01### # -*- coding: utf-8 -*-
-###copy-file-op01### # Python program to explain shutil.copy() method
-###copy-file-op01###
-###copy-file-op01### # importing shutil module
-###copy-file-op01### import shutil
-###copy-file-op01###
-###copy-file-op01### # Source path
-###copy-file-op01### source = "/home/Michael/Documents/file.txt"
-###copy-file-op01###
-###copy-file-op01### # Destination path
-###copy-file-op01### destination = "/home/Michael/Documents/dest.txt"
-###copy-file-op01###
-###copy-file-op01### # Copy the content of
-###copy-file-op01### # source to destination
-###copy-file-op01###
-###copy-file-op01### try:
-###copy-file-op01###     shutil.copy(source, destination)
-###copy-file-op01###     print("File copied successfully.")
-###copy-file-op01###
-###copy-file-op01### # If source and destination are same
-###copy-file-op01### except shutil.SameFileError:
-###copy-file-op01###     print("Source and destination represents the same file.")
-###copy-file-op01###
-###copy-file-op01### # If there is any permission issue
-###copy-file-op01### except PermissionError:
-###copy-file-op01###     print("Permission denied.")
-###copy-file-op01###
-###copy-file-op01### # For other errors
-###copy-file-op01### except:
-###copy-file-op01###     print("Error occurred while copying file.")
-###copy-file-op01###
+'''
 
 
 
 
-###function-styple-map-reduce01### # https://www.liaoxuefeng.com/wiki/1016959663602400/1017329367486080
-###function-styple-map-reduce01### # -*- coding: utf-8 -*-
-###function-styple-map-reduce01### from datetime import datetime
-###function-styple-map-reduce01### from functools import reduce
-###function-styple-map-reduce01### DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
-###function-styple-map-reduce01###
-###function-styple-map-reduce01###
-###function-styple-map-reduce01### def str2int(s):   ###map-reduce function
-###function-styple-map-reduce01###
-###function-styple-map-reduce01###     def fn(x, y):
-###function-styple-map-reduce01###         return x * 10 + y
-###function-styple-map-reduce01###
-###function-styple-map-reduce01###     def char2num(s):
-###function-styple-map-reduce01###         return DIGITS[s]
-###function-styple-map-reduce01###     return reduce(fn, map(char2num, s))
-###function-styple-map-reduce01###
-###function-styple-map-reduce01###
-###function-styple-map-reduce01### def f(x):      ###map function 01
-###function-styple-map-reduce01###     return x * x
-###function-styple-map-reduce01###
-###function-styple-map-reduce01### def main():
-###function-styple-map-reduce01###     print (list(map(str,   [1, 2,  3,  4,  5,  6])))  ###map02,shift int to string
-###function-styple-map-reduce01###
-###function-styple-map-reduce01###     r = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])
-###function-styple-map-reduce01###     print (list(r))      ###map
-###function-styple-map-reduce01###
-###function-styple-map-reduce01###     print (str2int('15638'))   ###map-reduce call
-###function-styple-map-reduce01###
-###function-styple-map-reduce01### if __name__ == '__main__':
-###function-styple-map-reduce01###     main()
+'''
 
+#https://docs.python.org/3/library/shutil.html
+#https://www.geeksforgeeks.org/python-shutil-copy-method/
+# -*- coding: utf-8 -*-
+# Python program to explain shutil.copy() method
+###copy-file-op01###
+# importing shutil module
+import shutil
+###copy-file-op01###
+# Source path
+source = "/home/Michael/Documents/file.txt"
+###copy-file-op01###
+# Destination path
+destination = "/home/Michael/Documents/dest.txt"
+###copy-file-op01###
+# Copy the content of
+# source to destination
+###copy-file-op01###
+try:
+    shutil.copy(source, destination)
+    print("File copied successfully.")
+###copy-file-op01###
+# If source and destination are same
+except shutil.SameFileError:
+    print("Source and destination represents the same file.")
+###copy-file-op01###
+# If there is any permission issue
+except PermissionError:
+    print("Permission denied.")
+###copy-file-op01###
+# For other errors
+except:
+    print("Error occurred while copying file.")
+
+'''
+
+
+'''
+####Functional programming https://coolshell.cn/articles/10822.html
+
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from functools import reduce
+import functools   ####partial function must import
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
+
+def str2int(s):   ###map-reduce function
+
+    def fn(x, y):
+        return x * 10 + y
+
+    def char2num(s):
+        return DIGITS[s]
+    return reduce(fn, map(char2num, s))
+
+
+def f(x):      ###map function 01
+    return x * x
+
+def is_odd(n):
+    return n % 2 == 1
+
+def main():
+    r = map(f, [1, 32, 5, 6, 7, 8, 9])
+    print (list(r))      ###map
+
+    print (list(map(str,   [1, 2,  3,  4,  5,  6])))  ###map2,shift int to string
+    print (sorted([1,9,-34,62,32],key=abs))
+    print (sorted(['bob',  'about',    'Zoo',  'Credit'], key=str.lower,reverse=True))
+    print (list(filter(is_odd, [1,2,3,4,5])))
+    print (str2int('15638'))   ###map-reduce call
+
+    max2=functools.partial(max,10)    ###apend default parameter 10 to the max2 function,here result is 10
+    print (max2(5,6,9))
+
+    int2 = functools.partial(int,base=2)
+    print (int2('111100'))
+
+    print (list(map(lambda x: x+7,[1,3,9])))  ###like def fun(x) return x+7
+
+if __name__ == '__main__':
+    main()
+
+'''
